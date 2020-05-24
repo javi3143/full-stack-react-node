340aa74413d3d0a7d803225adab8734ee5754e0a
admn <j.ruizperez@dundee.ac.uk>
2020-05-24 18:36:46 +0200

    2.c.pract

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Person from './components/Person'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: 656925282}
-  ])
+  const [ persons, setPersons ] = useState([''])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [newFilter, setFilter] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
