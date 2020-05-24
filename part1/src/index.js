d0c95d023bf5e9c6863cebe71443d7a196997010
admn <j.ruizperez@dundee.ac.uk>
2020-05-24 11:20:52 +0200

    part2a

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,64 +1,57 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => <h1>{props.text}</h1>
-
-const ShowStatistics = (props) => {
-  if ((props.valueGood > 0) || (props.valueNeutral > 0) || (props.valueBad > 0)) {
-    return (
-      <div>
-        <Statistic text="good" expression={props.valueGood}/>
-        <Statistic text="neutral" expression={props.valueNeutral}/>
-        <Statistic text="bad" expression={props.valueBad}/>
-        <Statistic text="average" expression={(props.valueGood - props.valueBad) / (props.valueGood + props.valueNeutral + props.valueBad)}/>
-        <Statistic text="positive" expression={props.valueGood / (props.valueGood + props.valueNeutral + props.valueBad)}/>
-      </div>
-    )
-  }
-  return (
-    <div>
-      <p>No feedback given</p>
-    </div>
-  )
-}
-
-const Statistic = (props) => (
-  <p>{props.text} {props.expression}</p>
-)
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
-)
+import Course from './components/Course'
 
 const App = () => {
-  const [valueGood, setValueGood] = useState(0)
-  const [valueNeutral, setValueNeutral] = useState(0)
-  const [valueBad, setValueBad] = useState(0)
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const setToValueGood = (newValue) => {
-    setValueGood(newValue)
-  }
-
-  const setToValueNeutral = (newValue) => {
-    setValueNeutral(newValue)
-  }
-
-  const setToValueBad = (newValue) => {
-    setValueBad(newValue)
-  }
-
-  return (
-    <div>
-      <Header text="give feedback"/>
-      <Button handleClick={() => setToValueGood(valueGood + 1)} text="good"/>
-      <Button handleClick={() => setToValueNeutral(valueNeutral + 1)} text="neutral"/>
-      <Button handleClick={() => setToValueBad(valueBad + 1)} text="bad"/>
-      <Header text="statistics"/>
-      <ShowStatistics valueGood={valueGood} valueNeutral={valueNeutral} valueBad={valueBad}/>
-    </div>
-  )
+  return <Course courses={courses} />
 }
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 )
