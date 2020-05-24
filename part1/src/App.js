d7c1a10116cb490dc6cdff48ba8fe89463ad786b
admn <j.ruizperez@dundee.ac.uk>
2020-05-24 19:21:49 +0200

    2.c.pract_2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,56 +1,29 @@
 import React, { useState, useEffect } from 'react'
-import Person from './components/Person'
+import Country from './components/Country'
+import Language from './components/Language'
 import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([''])
-
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+  const [ countries, setCountries ] = useState([''])
   const [showAll, setShowAll] = useState(true)
   const [newFilter, setFilter] = useState('')
 
   const hook = () => {
     console.log('effect')
     axios
-      .get('http://localhost:3001/persons')
+      .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setCountries(response.data)
       })
   }
   
   useEffect(hook, [])
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      name: newName,
-      number: newNumber
-    }
-    if (persons.some(({name}) => name === noteObject.name)) {
-      window.alert(noteObject.name + "is already added to phonebook")
-    } else {
-      setPersons(persons.concat(noteObject))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
-
-  const peopleToShow = 
+  const countryToShow = 
   showAll
-  ? persons
-  : persons.filter(p => p.name.includes(newFilter))
-
-  const handlePersonChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handlePersonNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+  ? countries
+  : countries.filter(p => p.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   const handleFilter = (event) => {
     console.log(event.target.value)
@@ -58,86 +31,48 @@ const App = () => {
     setShowAll(false)
   }
 
+  if (countryToShow.length > 10) {
+    return (
+      <div>
+        <div>
+            find countries: <input value={newFilter}
+            onChange={handleFilter}/>
+        </div>
+        <p>Too many matches, specify another filter</p>
+    </div>
+    )
+  } else if (countryToShow.length === 1) {
 
+    return (
+      <div>
+        <div>
+            find countries: <input value={newFilter}
+            onChange={handleFilter}/>
+        </div>
+        <h1>{countryToShow[0].name}</h1>
+        <p>capital {countryToShow[0].capital}</p>
+        <p>population {countryToShow[0].population}</p>
+
+        {/* <h1>languages</h1>
+        {countryToShow.map(c => c.languages).map(l => 
+          <Language language={l} />
+        )} */}
+
+        <img width="200" src={countryToShow[0].flag}/>
+      </div>
+    )
+  }
   return (
     <div>
-      <h2>Phonebook</h2>
       <div>
-          filter shown with: <input value={newFilter}
+          find countries: <input value={newFilter}
           onChange={handleFilter}/>
       </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName}
-          onChange={handlePersonChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber}
-          onChange={handlePersonNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {peopleToShow.map(p => 
-          <Person key={p.name} person={p} />
+      {countryToShow.map(p => 
+          <Country key={p.name} country={p} />
       )}
     </div>
   )
 }
 
 export default App
-
-// const App = (props) => {
-//   const [notes, setNotes] = useState(props.notes)
-//   const [newNote, setNewNote] = useState("")
-//   const [showAll, setShowAll] = useState(true)
-
-//   const addNote = (event) => {
-//     event.preventDefault()
-//     const noteObject = {
-//       content: newNote,
-//       date: new Date().toISOString(),
-//       important: Math.random() < 0.5,
-//       id: notes.length + 1,
-//     }
-  
-//     setNotes(notes.concat(noteObject))
-//     setNewNote('')
-//   }
-
-//   const handleNoteChange = (event) => {
-//     console.log(event.target.value)
-//     setNewNote(event.target.value)
-//   }
-
-//   const notesToShow = showAll
-//   ? notes
-//   : notes.filter(note => note.important === true)
-
-//   return (
-//     <div>
-//       <h1>Notes</h1>
-//       <div>
-//         <button onClick={() => setShowAll(!showAll)}>
-//           show {showAll ? 'important' : 'all' }
-//         </button>
-//       </div>
-//       <ul>
-//         {notesToShow.map(note => 
-//           <Note key={note.id} note={note} />
-//         )}
-//       </ul>
-//       <form onSubmit={addNote}>
-//         <input 
-//           value={newNote}
-//           onChange={handleNoteChange} />
-//         <button type="submit">save</button>
-//       </form>  
-//     </div>
-//   )
-// }
-
-// export default App 
---
+++ part1/src/components/Language.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Language = ({ language }) => {
+  return (
+    <p>{language.name}</p>
+  )
+}
+
+export default Language
